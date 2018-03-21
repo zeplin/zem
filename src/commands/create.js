8cd7c1439f6902f458e82887a8b35257895a2fbe
Muhammet Alay <muhammet@zeplin.io>
2018-03-21 16:39:45 +0300

    Spawn with shell set to true and not use npm.cmd on windows

--- src/commands/create.js
+++ src/commands/create.js
@@ -8,7 +8,7 @@ const packageName = "zem";
 function installDeps() {
     return new Promise((resolve, reject) => {
         const npmArgs = ["install", "--save", packageName];
-        const child = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", npmArgs, { stdio: "inherit" });
+        const child = spawn("npm", npmArgs, { stdio: "inherit", shell: true });
 
         child.on("close", exitCode => {
             if (exitCode !== 0) {
