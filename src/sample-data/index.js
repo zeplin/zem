84778904373e334f44b3c91d81756c07edbe2838
Sertac Olgunsoylu <sertac@zeplin.io>
2021-01-07 16:07:23 -0800

    Run screen and component functions within the `exec` command

--- src/sample-data/index.js
+++ src/sample-data/index.js
@@ -1,7 +1,13 @@
 const project = require("./project.json");
 const version = require("./version.json");
+const screens = require("./screens.json");
+const components = require("./components.json");
+const componentVariants = require("./componentVariants.json");
 
 module.exports = {
+    componentVariants,
+    components,
     project,
-    version
+    screens,
+    screenVersion: version
 };
